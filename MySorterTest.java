d55a9747f35d7c1f8e5a2e4e08028ca7e337437f
Danis0307 <daniss0307@gmail.com>
2016-12-27 22:46:42 +0800

    Testing

--- MySorterTest.java
+++ MySorterTest.java
@@ -24,6 +24,7 @@ public class MySorterTest {
 
 	private MySorter sorter;
 
+	
 	private void assertArrayEquals(double[] expected, double[] actual) {
 		assertEquals(expected.length, actual.length);
 		for (int i = 0; i < expected.length; i++) {
